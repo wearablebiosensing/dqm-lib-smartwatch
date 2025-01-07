d0125ff08394bde454fab4025b3ee3108b0af056
sadhushehjar <45221445+sadhushehjar@users.noreply.github.com>
2025-01-07 14:00:56 -0500

    imu code update.

--- dqm/src/dqm/__init__.py
+++ dqm/src/dqm/__init__.py
@@ -1,15 +1,11 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 import os
 import json
 import re
-import seaborn as sns
 from datetime import datetime
 from shutil import move
-import plotly.express as px
 import pytz
-import plotly.graph_objects as go
 
 
 ############################################################
@@ -45,9 +41,20 @@ def percentage_zeros_hr(df_hr):
     return percentage_zeros
 
 def snr_hr(df_hr):
-    return df_hr["bpm"].mean()/df_hr["bpm"].std()
+     
+    try:
+        snr_value =df_hr["bpm"].mean()/df_hr["bpm"].std()
+        # Code that might raise an error
+        # result = 10 / 0  # This will raise ZeroDivisionError
+    except ZeroDivisionError as e:
+        snr_value = -1
+        # Handle the specific error
+        print(f"Error occurred: {e}")
+
+    return snr_value
 
 def percentage_missing_hr(df):
+
     # Create DataFrame
     df = pd.DataFrame(df)
     # Convert watch_timestamp to datetime
@@ -63,3 +70,39 @@ def percentage_missing_hr(df):
     percentage_missing=  ((expected_number_of_samples-recieved_number_of_samples)/expected_number_of_samples)*100
     print("Percentage of missing data: ", percentage_missing)
     return percentage_missing
+
+# ['x(m/s^2)', 'y(m/s^2)', 'z(m/s^2)', 'internal_ts', 'watch_timestamp',
+# 'relative_timestamp', 'Session_ID', 'PID', 'ts_only', 'ts_seconds']
+                        
+def snr_gyro(df_gry):
+    snr_gyr_x = df_gry["x(rad)"].mean()/df_gry["x(rad)"].std()
+    snr_gyr_y = df_gry["y(rad)"].mean()/df_gry["y(rad)"].std()
+    snr_gyr_z = df_gry["z(rad)"].mean()/df_gry["z(rad)"].std()
+    return snr_gyr_x ,snr_gyr_y,snr_gyr_z
+def snr_acc(df_acc):
+
+    snr_acc_x = df_acc["x(m/s^2)"].mean()/df_acc["x(m/s^2)"].std()
+    snr_acc_y = df_acc["y(m/s^2)"].mean()/df_acc["y(m/s^2)"].std()
+    snr_acc_z = df_acc["z(m/s^2)"].mean()/df_acc["z(m/s^2)"].std()
+    return snr_acc_x,snr_acc_y,snr_acc_z
+
+
+def percentage_missing_imu(df):
+
+    # Create DataFrame
+    df = pd.DataFrame(df)
+    # Convert watch_timestamp to datetime
+    df['watch_timestamp'] = pd.to_datetime(df['watch_timestamp'])
+    # Generate a complete time index.
+    start_time = df.iloc[0]["watch_timestamp"] #df['watch_timestamp'].min()
+    end_time = df.iloc[-1]["watch_timestamp"] #df['watch_timestamp'].max()
+    print("start_time,end_time: ",start_time,end_time)
+    full_time_index = pd.date_range(start=start_time, end=end_time, freq='33.33ms')
+    print("df: ",df.shape[0],"len(full_time_index): ",len(full_time_index))
+    expected_number_of_samples = len(full_time_index)
+    recieved_number_of_samples = df.shape[0]
+    percentage_missing=  ((expected_number_of_samples-recieved_number_of_samples)/expected_number_of_samples)*100
+    print("Percentage of missing data: ", percentage_missing)
+    return percentage_missing
+
+
